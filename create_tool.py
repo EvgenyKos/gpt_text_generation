c951cdb90b06b76e449fdc593b125055c8b04400
EvgenyKos <evkostromskoy@gmail.com>
2022-02-10 22:08:40 -0500

    add desc

--- create_tool.py
+++ create_tool.py
@@ -12,8 +12,8 @@ def app(key):
 
     st.title("Create tool")
     form = st.form('Create tool')
-    tool_name = form.text_input("Tool name", "for example: Website headline")
-    prompt = form.text_area("Type the prompt to genarte text", height=200)
+    tool_name = form.text_input("Tool name", "for example: Digital Ad")
+    prompt = form.text_area("Type the prompt to genarte text", "for example: Write a creative ad for Learning Room that is  is a virtual environment to help students from kindergarten to high school excel in school focused on parents. ", height=200)
 
     if form.form_submit_button("Generate text"):
         with st.spinner("Generating text..."):
